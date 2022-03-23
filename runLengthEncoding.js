d0d53f58d608c2d5597f5f2a5f62d8430f38173e
Emmanuel Mota <emmanuelmota@gmail.com>
2022-03-22 21:09:17 -0700

    Spelling fixes

--- runLengthEncoding.js
+++ runLengthEncoding.js
@@ -13,30 +13,55 @@
 //imgBuilder Produces a string representing an image.
 // It does this by creating a string using a random letter from the alphabet, repeated a random number of times.. /// For example = 'aaabbbuiuuddddd'
 //
-var imgBuilder = () => {
+let imgBuilder = () => {
 	// Here I am declaring that the variable I am calling "alphabet" should be equal to the value to the right of the equals sign. In this case it's a data structure called an array, see those square brackets. That means its an array. In this array are a bunch of items, in this case it's the letters of... The Alphabet!
-	var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+	let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 	// Next lets create an empty array to store letters in.
-	var storageArr = [];
+	let storageArr = [];
 	// This is called a "For loop", within the parenthesis () are three expressions, each one is seperated by a semicolon these are called - (INITIALIZATION; CONDITION; FINAL-EXPRESSION) In the INITIALIZATION expression I declare that "i" will be equal to 0... That's all. Now to our middle CONDITION expression: The loop will not proceed if the CONDITION is not TRUE. We just set i to equal 0, and the length of our alphabet array is greater than or equal to i. Meaning this CONDITION evaluates to TRUE. So the loop runs the code  inside the loop Immediately. AFTER the code inside the loop is run, the loop looks at the last condition called the FINAL-EXPRESSION  i++ means increment i by 1. i is now equal to 1!
 	//The loop just completed the first cycle, at this point it goes straight to the CONDITION, if its true? Yup it runs the code inside the loop. But we just skipped the INITIALIZATION statement! Yes that statement is only looked on on the first run of the loop. The cycle continues until the CONDITION is no longer true EG i > the length of the alphabet array.  Congratulations you just learned one of the most commonly used patterns in programming!
 	for (let i = 0; i <= alphabet.length; i ++) {
-		//We are now inside the loop!
-		// Produce random number which we will use to select a letter at random form the alphabet
+		//I am now speaking to you from inside the for loop. Remember this code will only run if the ?? Yup if the CONDITION is TRUE.
+		// Lets produce a random number which we will in the next lines of code..lets leave this line unexplained for now..
 		let randomNumber = Math.floor(Math.random() * (alphabet.length - 1) + 1);
-		// Select a random Character from the alphabet array at the position in the array chosen by randomNumber
+		// Lets make a variable equal to a random Character from the alphabet array, at the position in the array chosen by randomNumber we created in the line above
+		// see the brackets after alphabet? we can pick a specific item from an array by entering a number there. This number is reffered to as an INDEX. So we just chose an item from alphabet with a .... Random Index. Which letter will it be?! Stay tuned.
 		 let randomChar = alphabet[randomNumber];
-		//Create a random number between 1 and 10
+		// I want to define how many times a letter repeats. I only wnat them to repeat say... 10 times maximum.
+		// So I create a variable called randomMaxTen, and use the .random() method (You will learn more about methods another time) and define that it should be a max of 10 and a minimum of 1.
+		// This will make randomMaxTen = a random number between 1 and 10
 		let randomMaxTen = Math.floor(Math.random() * (10 - 1) + 1);
-		// Push that random Character to storage Array and repeat that letter a random number of times (between 1 and 10) in a row
+
+		// OK we are almost done!
+		// In Javascript things inside parenthisis are evaluated first () and when a parenthesis is inside of a parenthisis like (A(B))
+		// B gets evaluated 1st and A gets evaluated 2nd.
+		// in looking at the line below what gets evaluated first ? randomMaxTen does! Lets say randomMaxTen happens to evaluate to 7 for this example.
+		// next up is the outer parenthisis, lets say randomChar evaluated to E. See that .repeat part? its calling the repeat method. And since we decided that randomMaxTen is = 7. Then everything inside the Parenthesis to the right of .push will evaluate to...
+		// Yup you guessed it EEEEEEE
+		// Lets use the .push method to PUSH those letters inside of the storageArr. And now storageArr = ['EEEEEEE'];
 		storageArr.push(randomChar.repeat(randomMaxTen));
 	}
-	// return the built storage array in the form of a combined string using the join method.
+	// If you made it here. You are amazing!
+	// Final line of code of this function as is with most functions is the return statement.
+	// This line of code is just that. What should this code RETURN or OUTPUT
+	// Since we are trying to make a representation of data in the form of a string like: "aaabbbecc"
+	// We should return a string.
+	// Remember how our storageArr is an ARRAY?  But we want a string right?
+	// Well thankfully there is a METHOD called .join which will take an array and combine all the items into the form of a string.
+	// Great! So whats inside the parenthisis? in the case of this .join method, what's inside is called a PARAMETER.
+	// See methods are functions themselves! This are part of javascript. And are VERY useful.
+	// So METHODS are just FUNCTIONS that we dont have to program. Sweet!!
+	// The .join method takes a PARAMETER called SEPERATOR it defines what to seperate the items by. Since there is nothing between the quotes, yup I dont want them seperated at all!
+	// So for example if I coded: return storageArr.join('x')
+	// the code would for example return "ExExExE"
+	// If I used a space... "E E E E"
+	// Ok so at this point the line below would return something like this: "aaabjjjjssdddkkkkk"
 	return storageArr.join('');
+	// Congrats you now know how a random data generating algorithm works!
 }
 
 // Percentage compressed Algorithm - takes initial length of uncompressed string and the output length of compressed string and does some basic math
-var percentageCompressed = (inputLength, outputLength) => {
+let percentageCompressed = (inputLength, outputLength) => {
 	let difference = inputLength - outputLength;
 	let average = difference / inputLength;
 	// Returns the average times 100 for a percentage and limits it to 4 characters EG: 22.42%
