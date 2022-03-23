b2f1adda783df2ebc6007cc36b810bf17c9c6fbe
Emmanuel Mota <emmanuelmota@gmail.com>
2022-03-22 20:28:30 -0700

    Initial draft of:
    Function imgBuilder
      Arrays.
      For Loop.
      randomNumber.
      randomChar.
      randomMaxTen.
      Pushing.
      Methods.
      Join
      Parameters.
      Return.

--- runLengthEncoding.js
+++ runLengthEncoding.js
@@ -1,21 +1,28 @@
-// This is the replication of a very old image compression algorithm.
-// it takes the representation of a JPG in the form of a string EG: 'aaabbbuiuuddddd'
-// And counts the number of times IN A ROW a letter appears.
-// It then appends that letter and number of times into a string and finally combines all values into
-// A compressed format. Basically it makes this:'aaabbbuiuuddddd' into this: 'a3b3u1i1u2d5'
+// This is the replication of a very old data compression algorithm called "Run Length Encoding"
+//https://en.wikipedia.org/wiki/Run-length_encoding#:~:text=Run%2Dlength%20encoding%20(RLE),than%20as%20the%20original%20run
 
+// In this example we will be creating a "Image" compression algorithm.
+// The function COMPRESS will take the representation of a JPG in the form of a string EG: 'aaabbbuiuuddddd'
+// it will count the number of times in a row a letter appears.
+// It will then combine the letter and number of times in a row it appears into EG: 'a1
+// Basically it makes this data :'aaabbbuiuuddddd' into this data: 'a3b3u1i1u2d5'
+// You are about to learn how a simple compression algorithm works! Let's go!
 
-//First we need an "Image" to compress:
-//imgBuilder Produces a string representing an  image.
-// It does this by creating a string using a random letter of alphabet, repeated a random number of times.. EG: 'aaabbbuiuuddddd'
+//First we need some data to compress:
+// Let's create an "Image"  using the function I called imgBuilder
+//imgBuilder Produces a string representing an image.
+// It does this by creating a string using a random letter from the alphabet, repeated a random number of times.. /// For example = 'aaabbbuiuuddddd'
+//
 var imgBuilder = () => {
-	// Yep its the alphabet..
+	// Here I am declaring that the variable I am calling "alphabet" should be equal to the value to the right of the equals sign. In this case it's a data structure called an array, see those square brackets. That means its an array. In this array are a bunch of items, in this case it's the letters of... The Alphabet!
 	var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-	// Create a storage array to push random letters too
+	// Next lets create an empty array to store letters in.
 	var storageArr = [];
-	// Loop the number of times there are letters in the alphabet array
+	// This is called a "For loop", within the parenthesis () are three expressions, each one is seperated by a semicolon these are called - (INITIALIZATION; CONDITION; FINAL-EXPRESSION) In the INITIALIZATION expression I declare that "i" will be equal to 0... That's all. Now to our middle CONDITION expression: The loop will not proceed if the CONDITION is not TRUE. We just set i to equal 0, and the length of our alphabet array is greater than or equal to i. Meaning this CONDITION evaluates to TRUE. So the loop runs the code  inside the loop Immediately. AFTER the code inside the loop is run, the loop looks at the last condition called the FINAL-EXPRESSION  i++ means increment i by 1. i is now equal to 1!
+	//The loop just completed the first cycle, at this point it goes straight to the CONDITION, if its true? Yup it runs the code inside the loop. But we just skipped the INITIALIZATION statement! Yes that statement is only looked on on the first run of the loop. The cycle continues until the CONDITION is no longer true EG i > the length of the alphabet array.  Congratulations you just learned one of the most commonly used patterns in programming!
 	for (let i = 0; i <= alphabet.length; i ++) {
-		// Produce random number  which we will use to select a letter at random form the alphabet
+		//We are now inside the loop!
+		// Produce random number which we will use to select a letter at random form the alphabet
 		let randomNumber = Math.floor(Math.random() * (alphabet.length - 1) + 1);
 		// Select a random Character from the alphabet array at the position in the array chosen by randomNumber
 		 let randomChar = alphabet[randomNumber];
@@ -38,7 +45,7 @@ var percentageCompressed = (inputLength, outputLength) => {
 
 // Encoding Algorithm ----
 // Takes string of uncompressed img EG: 'abbcccdddd' defined here as 'src'
-var encode = (src) => {
+var compress = (src) => {
 	//create storage array
 	var arr = [];
 	// Set intial variables to my defaults
@@ -83,4 +90,4 @@ var encode = (src) => {
 	return compressed;
 }
 //Run the Encoding Algorithm using the output of the imgBuilder Algorithm.
-encode(imgBuilder());
+compress(imgBuilder());
